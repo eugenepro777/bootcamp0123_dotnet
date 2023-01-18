fcb4560daf95d069a64cef56b903257744298ea1
Eugene_Pro <proskuryakov.e@list.ru>
2023-01-18 13:40:39 +0300

    add расширенная+многопоточная сортировка подсчетом

---
+++ CountingSort_v2/Program.cs
@@ -0,0 +1,161 @@
+﻿// сортировка подсчетом (простая + расширенная +многопоточная)
+
+const int THREADS_NUMBER = 16; // количество потоков
+const int N = 100000;// размер массива
+object locker = new object();
+
+
+// int[] array = { -10, -5, -9, 0, 2, 5, 1, 3, 1, 0, 1 };
+// int[] sortedArray = CountingSortExtended(array);
+Random rand = new Random();
+int[] resSerial = new int[N].Select(r => rand.Next(0, 5)).ToArray();
+// int[] resSerial = new int[N];
+int[] resParallel = new int[N];
+
+Array.Copy(resSerial, resParallel, N);
+
+// Console.WriteLine(string.Join(", ", resSerial));
+
+CountingSortExtended(resSerial);
+PrepareParallelCountingSort(resParallel);
+Console.WriteLine(EqualityMatrix(resSerial, resParallel));
+
+// Console.WriteLine(string.Join(", ", resSerial));
+// Console.WriteLine(string.Join(", ", resParallel));
+
+
+
+void PrepareParallelCountingSort(int[] inputArray)
+{
+    int max = inputArray.Max();
+    int min = inputArray.Min();
+
+    int offset = -min;
+    int[] counters = new int[max + offset + 1];
+
+    int eachThreadCalc = N / THREADS_NUMBER;
+    var threadsParallel = new List<Thread>();
+
+    for (int i = 0; i < THREADS_NUMBER; i++)
+    {
+        int startPos = i * eachThreadCalc;
+        int endPos = (i + 1) * eachThreadCalc;
+
+        if (i == THREADS_NUMBER - 1) endPos = N;
+        threadsParallel.Add(new Thread(() => CountingSortParallel(inputArray, counters, offset, startPos, endPos)));
+        threadsParallel[i].Start();
+    }
+
+    foreach (var thread in threadsParallel)
+    {
+        thread.Join();
+    }
+
+    int index = 0;
+    for (int i = 0; i < counters.Length; i++)
+    {
+        for (int j = 0; j < counters[i]; j++)
+        {
+            inputArray[index] = i - offset;
+            index++;
+        }
+    }
+}
+
+void CountingSortParallel(int[] inputArray, int[] counters, int offset, int startPos, int endPos)
+{
+    for (int i = startPos; i < endPos; i++)
+    {
+        lock (locker)
+        {
+            counters[inputArray[i] + offset]++;
+        }
+    }
+}
+
+void CountingSortExtended(int[] inputArray)
+{
+    int max = inputArray.Max();
+    int min = inputArray.Min();
+
+    int offset = -min;
+    int[] counters = new int[max + offset + 1];
+
+    for (int i = 0; i < inputArray.Length; i++)
+    {
+        counters[inputArray[i] + offset]++;
+    }
+
+    int index = 0;
+    for (int i = 0; i < counters.Length; i++)
+    {
+        for (int j = 0; j < counters[i]; j++)
+        {
+            inputArray[index] = i - offset;
+            index++;
+        }
+    }
+}
+
+// сравнение массивов, применяется и для матриц
+bool EqualityMatrix(int[] fmatrix, int[] smatrix)
+{
+    bool res = true;
+
+    for (int i = 0; i < N; i++)
+    {
+        res = res && (fmatrix[i] == smatrix[i]);
+    }
+    return res;
+}
+
+//Console.WriteLine(string.Join(",", sortedArray));
+
+// простая сортировка подсчетом
+/* void CountingSort(int[] inputArray)
+{
+    int[] counters = new int[10];
+
+    for (int i = 0; i < inputArray.Length; i++)
+    {
+        counters[inputArray[i]]++;
+    }
+
+    int index = 0;
+    for (int i = 0; i < counters.Length; i++)
+    {
+        for (int j = 0; j < counters[i]; j++)
+        {
+            inputArray[index] = i;
+            index++;
+        }
+    }
+} */
+
+// расширенная сортировка подсчетом
+/* int[] CountingSortExtended(int[] inputArray)
+
+{
+    int max = inputArray.Max();
+    int min = inputArray.Min();
+
+    int offset = -min;
+    int[] sortedArray = new int[inputArray.Length];
+    int[] counters = new int[max + offset + 1];
+
+    for (int i = 0; i < inputArray.Length; i++)
+    {
+        counters[inputArray[i] + offset]++;
+    }
+
+    int index = 0;
+    for (int i = 0; i < counters.Length; i++)
+    {
+        for (int j = 0; j < counters[i]; j++)
+        {
+            sortedArray[index] = i - offset;
+            index++;
+        }
+    }
+    return sortedArray;
+} */
